c2a1f6e067b936636d4d6cb67c32b61917df1f9c
Brayanc31 <brayanmarxo31@gmail.com>
2026-04-21 18:23:40 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,9 @@ import sys
 import os
 from antlr4 import *
 
-# Configuración de rutas para encontrar los archivos generados
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# Si tu estructura es src/main.py y src/antlr_gen/...
-gen_dir = os.path.join(current_dir, 'antlr_gen')
+# Ruta a la subcarpeta que contiene los generados
+gen_dir = os.path.join(current_dir, 'antlr_gen', 'grammar')
 sys.path.insert(0, gen_dir)
 
 try:
@@ -19,38 +18,25 @@ except ImportError as e:
     sys.exit(1)
 
 def main(path_script):
-    # Validar existencia del archivo
     if not os.path.exists(path_script):
         print(f"Error: El script '{path_script}' no existe.")
         return
-
     try:
-        # 1. Flujo de lectura
         input_stream = FileStream(path_script, encoding='utf-8')
-        
-        # 2. Análisis Léxico
         lexer = TintoLexer.TintoLexer(input_stream)
         stream = CommonTokenStream(lexer)
-        
-        # 3. Análisis Sintáctico
         parser = TintoParser.TintoParser(stream)
         tree = parser.program()
-        
-        # Verificar si hubo errores de sintaxis antes de ejecutar
         if parser.getNumberOfSyntaxErrors() > 0:
-            print(f" Se encontraron {parser.getNumberOfSyntaxErrors()} errores sintácticos.")
+            print(f"Se encontraron {parser.getNumberOfSyntaxErrors()} errores sintácticos.")
             return
-
-        # 4. Ejecución con el Visitor
         visitor = TintoInterpreter()
         visitor.visit(tree)
-        
     except Exception as e:
         print(f"Error durante la ejecución: {e}")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        # Ejemplo de uso si no se pasan argumentos
         print("Uso sugerido: python main.py scripts/prueba.tinto")
     else:
         main(sys.argv[1])
